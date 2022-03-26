9f40846a6c8265f5e2c973cb8b2a001c113f74df
OtherJohnGray <john@allaiz.com>
2022-03-26 00:49:48 -0400

    add read tests, support KiB and GiB results, synch and throughput

--- ashiftio.sh
+++ ashiftio.sh
@@ -44,7 +44,7 @@ echo
 
 # display runtime
 ashift_count=${#ashifts[@]}
-((test_count=$ashift_count*3))
+((test_count=$ashift_count*6))
 ((runtime=test_count*$seconds/60))
 echo ""
 echo ""
@@ -56,9 +56,12 @@ echo "starting tests...."
 # set up variables
 cwd=$(pwd)
 commands=(
-  "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=4k --size=4g --numjobs=1 --iodepth=1 --runtime=$seconds --time_based --end_fsync=1"
-  "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=64k --size=256m --numjobs=16 --iodepth=16 --runtime=$seconds --time_based --end_fsync=1"
-  "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=1m --size=16g --numjobs=1 --iodepth=1 --runtime=$seconds --time_based --end_fsync=1"
+  "fio --name=random-read  --ioengine=posixaio --rw=randread --bs=4k --size=40m --numjobs=1 --iodepth=1 --runtime=$seconds --time_based"
+  "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=4k --size=4g --numjobs=1 --iodepth=1 --runtime=$seconds --time_based"
+  "fio --name=random-read  --ioengine=posixaio --rw=randread --bs=64k --size=40m --numjobs=16 --iodepth=16 --runtime=$seconds --time_based"
+  "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=64k --size=256m --numjobs=16 --iodepth=16 --runtime=$seconds --time_based"
+  "fio --name=random-read  --ioengine=posixaio --rw=randread --bs=1m --size=40m --numjobs=1 --iodepth=1 --runtime=$seconds --time_based"
+  "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=1m --size=16g --numjobs=1 --iodepth=1 --runtime=$seconds --time_based"
 )
 recordsizes=( 4K 64K 1M )
 declare -a results
@@ -67,32 +70,45 @@ declare -a results
 for i in "${ashifts[@]}"
 do
   echo "creating testpool with ashift of $i"
-	zpool create -o ashift=$i -O acltype=posixacl -O compression=lz4 -O dnodesize=auto -O normalization=formD -O relatime=on -O xattr=sa -O mountpoint=none testpool $disk || exit 1
+	zpool create -o ashift=$i testpool $disk || exit 1
   for j in {0..2}
   do
     recordsize=${recordsizes[j]}
-    command=${commands[j]}
     echo "creating dataset with recordsize of $recordsize"
-    zfs create -o recordsize=$recordsize -o mountpoint=/testpool/$recordsize testpool/$recordsize || exit 1
+    zfs create -o acltype=posixacl -o dnodesize=auto -o normalization=formD -o atime=off -o xattr=sa -o primarycache=none -o logbias=throughput -o sync=always -o recordsize=$recordsize -o mountpoint=/testpool/$recordsize testpool/$recordsize || exit 1
     echo "changing to dataset directory"
     cd /testpool/$recordsize
-    echo "running fio command for $seconds seconds: $command"
-    if [[ $verbose -eq 1 ]] ; then
-      res=$(eval "$command | tee /dev/tty /tmp/ashiftstat.txt | tail -1 || exit 1")
-    else
-      res=$(eval "$command | tail -1 || exit 1")
-    fi
-    pat='WRITE: bw=([0-9.]*)MiB/s'
-    if [[ "$res" =~ $pat ]]; then
-      results+=( ${BASH_REMATCH[1]} )
-    else
-      echo ""
-      echo "ERROR:"
-      echo "Could not match output pattern of $pat"
-      echo "Is fio reporting bw= in something other than MiB/s ?"
-      echo "fio output was: $res"
-      exit 1
-    fi
+    for k in {0..1}
+    do
+      ((command_idx=j*2+k))
+      command=${commands[command_idx]}
+      echo "running fio command for $seconds seconds: $command"
+      if [[ $verbose -eq 1 ]] ; then
+        res=$(eval "$command | tee /dev/tty /tmp/ashiftstat.txt | tail -1 || exit 1")
+      else
+        res=$(eval "$command | tail -1 || exit 1")
+      fi
+      pat=': bw=([0-9]*).?([0-9]*)?(KiB|MiB|GiB)/s'
+      if [[ "$res" =~ $pat ]]; then
+        value=${BASH_REMATCH[1]}
+        units=${BASH_REMATCH[3]}
+        if [ "$units" = "KiB" ]; then
+          ((mib=$value/1024))
+        elif [ "$units" = "GiB" ]; then
+          ((mib=$value*1024))
+        else
+          ((mib=$value))
+        fi
+        results+=( $mib )
+      else
+        echo ""
+        echo "ERROR:"
+        echo "Could not match output pattern of $pat"
+        echo "Is fio reporting bw= in something other than KiB/s, MiB/s, or GiB/s ?"
+        echo "fio output was: $res"
+        exit 1
+      fi
+    done
     echo "changing back to original working directory"
     cd $cwd
     echo "destroying $recordsize recordsize dataset"
@@ -109,16 +125,19 @@ eval printf %.0s- '{1..'"$underscore_count"\}
 printf "\nWrite performance in MiB/s at various ashifts for $disk\n"
 eval printf %.0s- '{1..'"$underscore_count"\}
 printf "\n\n"
-printf "%6s %10s %10s %10s \n" "ASHIFT" ${recordsizes[0]} ${recordsizes[1]} ${recordsizes[2]}
-printf %.0s= {1..39}
+printf "%6s %10s %10s %10s %10s %10s %10s\n" "ASHIFT" "${recordsizes[0]} read" "${recordsizes[0]} write" "${recordsizes[1]} read" "${recordsizes[1]} write" "${recordsizes[2]} read" "${recordsizes[2]} write" 
+printf %.0s= {1..72}
 ((endindex=$ashift_count-1))
 for i in $(seq 0 $endindex)
 do
   echo 
-  ((first=$i*3))
+  ((first=$i*6))
   ((second=$first+1))
   ((third=$second+1))
-  printf "%6d %10.0f %10.0f %10.0f \n" ${ashifts[$i]} ${results[$first]} ${results[$second]} ${results[$third]}
+  ((fourth=$third+1))
+  ((fifth=$fourth+1))
+  ((sixth=$fifth+1))
+  printf "%6d %10.0f %10.0f %10.0f %10.0f %10.0f %10.0f \n" ${ashifts[$i]} ${results[$first]} ${results[$second]} ${results[$third]} ${results[$fourth]} ${results[$fifth]} ${results[$sixth]}
 done
 printf "\n\n\n"
 
